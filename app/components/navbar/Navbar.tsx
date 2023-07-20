9607c13679c7fd880400cb12e82c23dbbd6b5567
Webees Tech <webeestechfr@gmail.com>
2023-07-20 11:27:09 -0700

    navbar2

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -3,6 +3,7 @@
 import Container from "../Container"
 import Logo from "./Logo"
 import Search from "./Search"
+import UserMenu from "./UserMenu"
 
 const Navbar = () => {
   return (
@@ -12,6 +13,7 @@ const Navbar = () => {
                 <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
                     <Logo />
                     <Search />
+                    <UserMenu />
                 </div>
             </Container>
         </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import ClientOnly from './components/ClientOnly'
 import Navbar from './components/navbar/Navbar'
 import './globals.css'
 import { Nunito } from 'next/font/google'
@@ -17,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={nunito.className}>
-        <Navbar />
+        <ClientOnly>
+          <Navbar />
+        </ClientOnly>
         {children}
         </body>
     </html>
